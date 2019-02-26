a077a2c178cddbcbed7ff3f890c2ca8e8166623c
yuanshuai1995 <369271498@qq.com>
2019-02-26 09:25:06 +0800

    增加swagger支持

--- src/main/java/com/yuan/erp/controller/UserController.java
+++ src/main/java/com/yuan/erp/controller/UserController.java
@@ -3,17 +3,21 @@ package com.yuan.erp.controller;
 import com.yuan.erp.entity.User;
 import com.yuan.erp.service.IUserService;
 import com.yuan.erp.util.RedisUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-@RestController
+@RestController("/user")
 @EnableAutoConfiguration
+@Api(value = "userController", tags = "用户信息")
 public class UserController {
 
     @Autowired
@@ -21,13 +25,15 @@ public class UserController {
     @Resource
     RedisUtil redisUtil;
 
-    @RequestMapping("/user/getAll")
+    @ApiOperation(value = "获取所有用户", httpMethod = "GET")
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     public List<User> getAllUsers() {
 
         return userService.getAllUser();
     }
 
-    @RequestMapping("/user/insert")
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    @ApiOperation(value = "新增用户", httpMethod = "POST")
     public boolean insertUser(User user) {
         try {
             userService.insertUser(user);
@@ -38,7 +44,8 @@ public class UserController {
         return false;
     }
 
-    @RequestMapping("/user/update/{userId}")
+    @RequestMapping(value = "/update/{userId}", method = RequestMethod.POST)
+    @ApiOperation(value = "更新用户", httpMethod = "POST")
     public boolean updateUser(User user, @PathVariable String userId) {
         try {
             userService.updateUser(user);
@@ -49,7 +56,8 @@ public class UserController {
         return false;
     }
 
-    @RequestMapping("/user/delete/{userId}")
+    @RequestMapping(value = "/delete/{userId}", method = RequestMethod.POST)
+    @ApiOperation(value = "删除用户", httpMethod = "POST")
     public boolean deleteUser(@PathVariable String userId) {
         try {
             userService.deleteUser(userId);
